7f72fe91e174394b95b3bd77f9aab9cfcab3fc15
2 <morovo1988@gmail.com>
2019-05-12 21:03:59 +0300

    4th commit improve code (improve  delete user, delete monthBudget)

--- src/main/java/com/gmail/morovo1988/budjet/config/MvcConfig.java
+++ src/main/java/com/gmail/morovo1988/budjet/config/MvcConfig.java
@@ -8,41 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("/security/login");
-        registry.addViewController("/").setViewName("home");
-    }
-
-
+//    @Override
 //    public void addViewControllers(ViewControllerRegistry registry) {
-//        registry.addViewController("/home").setViewName("home");
+//        registry.addViewController("/login").setViewName("/security/login");
 //        registry.addViewController("/").setViewName("home");
-//        registry.addViewController("/hello").setViewName("hello");
-//        registry.addViewController("/login").setViewName("login");
-//    }
-
-//
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/styles/**").addResourceLocations("/styles/styles");
-//    }
-
-//    @Bean
-//    public UrlBasedViewResolver setupViewResolver() {
-//        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
-//        resolver.setPrefix("/templates/pages/");
-//        resolver.setSuffix(".jsp");
-//        resolver.setViewClass(JstlView.class);
-//
-//        return resolver;
-//    }
-//    @Override
-//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//
-//        registry.addResourceHandler("/styles/**").addResourceLocations("/WEB-INF/styles/");
-//
-//
-//
 //    }
 }
